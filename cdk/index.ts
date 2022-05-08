32ed0663db6bbad8a9ad54f3ff30393cd96644b8
Eric Kelly <github@useric.com>
2022-05-07 18:33:53 -0700

    Deploy works to create Cloud9 environment

--- cdk/index.ts
+++ cdk/index.ts
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
 
-import { Stack, App, aws_cloud9 as cloud9 } from 'aws-cdk-lib';
+import { Stack, 
+         App, 
+         CfnOutput,
+         aws_ec2 as ec2, 
+         aws_cloud9 as cloud9, 
+         aws_iam as iam } from 'aws-cdk-lib';
+import * as c9 from "@aws-cdk/aws-cloud9-alpha"
 
+// App
 const app = new App();
-const stack = new Stack(app, 'DevelopmentStack');
 
-const cfnEnvironmentEC2 = new cloud9.CfnEnvironmentEC2(stack, 'MyCfnEnvironmentEC2', {
+// Constants
+const envUSA = { account: '804862212009', region: 'us-west-2' };
+
+// Dev stack
+const stack = new Stack(app, 'DevelopmentStack', { env: envUSA });
+
+const developer = new iam.User(stack, 'developer');
+const admin = iam.User.fromUserName(stack, 'MyImportedUserByName', 'admin');
+
+const c9env = new cloud9.CfnEnvironmentEC2(stack, 'MyCloud9EC2Environment', {
   instanceType: stack.node.tryGetContext('instanceType'),
 
   // the properties below are optional
-  automaticStopTimeMinutes: 30,
   description: 'Developer coding environment',
   name: stack.node.tryGetContext('name'),
+  automaticStopTimeMinutes: 30,
+  ownerArn: admin.userArn,
+  tags: [],
 });
 
 app.synth();
