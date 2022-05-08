00e47ac255ff1d1ff4c384b541b2ce49c7b15125
Eric Kelly <github@useric.com>
2022-05-08 05:48:16 +0000

    Fixed cloud9 setup

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -2,3 +2,7 @@
 
 git config --global core.editor vim
 ssh-keygen
+
+read -n1 -r -p "Upload key to Github, then press any key to continue..." key
+
+git remote set-url origin git@github.com:ekelly/onebusaway.git
