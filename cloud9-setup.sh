bfc65d1f44e1fbf7b68e30adc096b7f4a6f3d973
Eric Kelly <github@useric.com>
2022-05-08 05:50:08 +0000

    Print generated SSH key

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -3,6 +3,11 @@
 git config --global core.editor vim
 ssh-keygen
 
+echo "SSH KEY:"
+echo ""
+cat ~/.ssh/id_rsa.pub
+echo ""
+
 read -n1 -r -p "Upload key to Github, then press any key to continue..." key
 
 git remote set-url origin git@github.com:ekelly/onebusaway.git
