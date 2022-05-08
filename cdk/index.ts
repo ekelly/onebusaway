376c64691e88a784898266f1f6eea896f04708c1
Eric Kelly <github@useric.com>
2022-05-08 15:51:43 -0700

    Things seem to be working again

--- cdk/index.ts
+++ cdk/index.ts
@@ -7,9 +7,9 @@ import { Stack,
          SecretValue,
          aws_ec2 as ec2, 
          aws_cloud9 as cloud9, 
+         aws_secretsmanager as secretsmanager,
          aws_iam as iam } from 'aws-cdk-lib';
 import * as c9 from "@aws-cdk/aws-cloud9-alpha";
-import { v4 as uuidv4 } from "uuid";
 
 // Constants
 const envUSA = { account: '804862212009', region: 'us-west-2' };
@@ -23,43 +23,53 @@ class DevelopmentStack extends Stack {
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName('AWSCloud9User'),
         iam.ManagedPolicy.fromAwsManagedPolicyName('IAMUserChangePassword'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('PowerUserAccess'),
       ]
     });
     
-    // Make the developer user if they do not already exist.
+    // Make the developer user
     const developerUsername = this.node.tryGetContext('developerUsername');
-    var user = iam.User.fromUserName(this, 'developer', developerUsername);
-    if (!user) {
-      const randomPassword = uuidv4();
-      const user = new iam.User(this, 'developer', {
-        userName: developerUsername,
-        password: SecretValue.unsafePlainText(randomPassword),
-        passwordResetRequired: true,
-        groups: [developersGroup]
-      });
+    const user = this.createDeveloperResources('developer', developerUsername, developersGroup);
+    developersGroup.addUser(user);
 
-      new CfnOutput(this, 'Developer username', {
-        value: user.userName,
-        description: "Developer account's username"
-      });
-      new CfnOutput(this, 'Developer password', {
-        value: randomPassword,
-        description: "Developer account's password"
-      });
-    }
+    // Get a reference to the Admin user
     const admin = iam.User.fromUserName(this, 'MyImportedUserByName', 'admin');
 
-    // Resources
-    const c9env = new cloud9.CfnEnvironmentEC2(this, 'MyCloud9EC2Environment', {
+    // General Resources
+    const c9env = new cloud9.CfnEnvironmentEC2(this, 'AdminCloud9EC2Environment', {
+      instanceType: this.node.tryGetContext('instanceType'),
+
+      // the properties below are optional
+      description: 'Admin coding environment',
+      name: 'admin-cloud9',
+      automaticStopTimeMinutes: 15,
+      ownerArn: admin.userArn,
+      tags: [{
+        "key": "user",
+        "value": admin.userName
+      }],
+    });
+  }
+
+  createDeveloperResources(id: string, username: string, developerGroup: iam.Group) {
+    // Fetch the user and make sure it is in the correct group
+    const user = iam.User.fromUserName(this, id, username);
+
+    // Create the resources for the developer
+    const c9env = new cloud9.CfnEnvironmentEC2(this, id+'Cloud9EC2Environment', {
       instanceType: this.node.tryGetContext('instanceType'),
 
       // the properties below are optional
       description: 'Developer coding environment',
       name: this.node.tryGetContext('name'),
-      automaticStopTimeMinutes: 30,
+      automaticStopTimeMinutes: 15,
       ownerArn: user.userArn,
-      tags: [],
+      tags: [{
+        "key": "user",
+        "value": user.userName
+      }],
     });
+    return user;
   }
 }
 
