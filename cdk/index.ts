0ca2009d3ca50984b1d25ff98768f37f612df523
Eric Kelly <github@useric.com>
2022-05-29 16:17:10 -0700

    Fixed broken build

--- cdk/index.ts
+++ cdk/index.ts
@@ -1,16 +1,32 @@
 #!/usr/bin/env node
 
 import { Stack, StackProps, App, CfnOutput } from "aws-cdk-lib";
+import { Construct } from 'constructs';
 import { DevelopmentStack } from "./lib/development";
 import { PipelineStack } from "./lib/pipeline";
+import { BillingAlarm } from 'aws-cdk-billing-alarm';
 
 // Constants
 const envUSA = { account: "804862212009", region: "us-west-2" };
+
+class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Create an alarm that emails `admin@example.com`
+    // if estimated charges exceed 1 USD
+    new BillingAlarm(this, 'AWSAccountBillingAlarm', {
+      monthlyThreshold: 1,
+      emails: ['awsonebusaway@useric.com'],
+    });
+  }
+}
 
 // App
 const app = new App();
 new DevelopmentStack(app, "DevelopmentStack", { env: envUSA });
 new PipelineStack(app, "PipelineStack", { env: envUSA });
+new CdkStack(app, "CdkStack", { env: envUSA });
 app.synth();
 
 console.log("Done synthesizing");
