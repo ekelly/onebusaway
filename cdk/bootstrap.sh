5d6665b5d6a0514ecf603c86d7aaa4b7efc52e82
Eric Kelly <github@useric.com>
2022-05-29 16:32:06 -0700

    Fixed bootstrap script

--- cdk/bootstrap.sh
+++ cdk/bootstrap.sh
@@ -3,4 +3,5 @@
 ACCOUNT_ID=`aws sts get-caller-identity --query "Account" --output text`
 REGION=`aws configure get region`
 
-cdk bootstrap aws://$ACCOUNT_ID/$REGION
+export CDK_NEW_BOOTSTRAP=1
+cdk bootstrap --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess aws://$ACCOUNT_ID/$REGION
