9b3707cc270d1f7b41d50b2dc625f161cdd42677
eric <onebusaway-eric@useric.com>
2022-06-02 02:54:50 +0000

    Added skill code

--- cdk/lib/pipeline.ts
+++ cdk/lib/pipeline.ts
@@ -6,45 +6,34 @@ import {
   SecretValue,
 } from "aws-cdk-lib";
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { PipelineSkillStage } from "./pipeline-skill-stage";
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // const sourceAction = new cp_actions.GitHubSourceAction({
-    //   actionName: "GitHub_Source",
-    //   owner: "awslabs",
-    //   repo: "aws-cdk",
-    //   oauthToken: process.env.GITHUB_OAUTH_TOKEN,
-    //   output: sourceOutput,
-    //   branch: "master", // default: 'master'
-    // });
     
     const skillStage = new PipelineSkillStage(this, "PipelineSkillStage");
+    const source = CodePipelineSource.gitHub("ekelly/onebusaway", 'main', {
+      authentication: SecretValue.secretsManager("prod/repository/apikey", {
+        jsonField: "github-token"
+      })
+    });
 
     const pipeline = new CodePipeline(this, "OneBusAwayPipeline", {
       pipelineName: "OneBusAway-Pipeline",
       crossAccountKeys: false,
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub("ekelly/onebusaway", 'main', {
-          authentication: SecretValue.secretsManager("prod/repository/apikey", {
-            jsonField: "github-token"
-          })
-        }),
+        input: source,
         primaryOutputDirectory: 'cdk/cdk.out',
         commands: ['cd cdk', 'npm ci', 'npm run build', 'cd ..']
-      }),
-
-      // stages: [
-      //   {
-      //     stageName: "Source",
-      //     actions: [sourceAction],
-      //   },
-      // ],
+      })
     });
     
-    pipeline.addStage(skillStage);
+    const stage = pipeline.addStage(skillStage);
+    stage.addPost(new ShellStep('validate', {
+      input: source,
+      commands: ['sh ./skill/tst/validate.sh']
+    }));
   }
 }
--- cdk/lib/skill-stack.ts
+++ cdk/lib/skill-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Function, InlineCode, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 
 export class SkillStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
-  
-      new Function(this, 'AlexaSkillLambdaFunction', {
-        runtime: Runtime.NODEJS_12_X,
-        handler: 'index.handler',
-        code: new InlineCode('exports.handler = _ => "Hello, CDK";')
-      });
-    }
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new Function(this, 'AlexaSkillLambdaFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: Code.fromAsset(`${process.cwd()}/../skill/dist/index.zip`)
+    });
+  }
 }
