06f2e9bdf2dd4992655ffdf11852b889ef38d431
eric <onebusaway-eric@useric.com>
2022-06-06 02:47:31 +0000

    Maybe works?

--- cdk/lib/skill-stack.ts
+++ cdk/lib/skill-stack.ts
@@ -1,15 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Function, InlineCode, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import {
+  Stack,
+  StackProps
+} from 'aws-cdk-lib';
+import * as path from 'path';
 
-export class SkillStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { Construct } from 'constructs';
+import { Function, InlineCode, Runtime, Code, AssetCode } from 'aws-cdk-lib/aws-lambda';
+
+export class SkillStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     new Function(this, 'AlexaSkillLambdaFunction', {
       runtime: Runtime.NODEJS_14_X,
       handler: 'index.handler',
-      code: Code.fromAsset(`${process.cwd()}/../skill/dist/index.zip`)
+      code: AssetCode.fromAsset(path.join(process.cwd(), "..", "skill"))
     });
   }
 }
